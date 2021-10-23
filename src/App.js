9064976d4eb83601079db75d662dc96eaa9b1049
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-10-23 23:21:52 +0530

    first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from "react";
+import "./App.css";
+import firebaseConfig from "./firebaseConfig";
+import * as firebaseui from "firebaseui";
+import firebase from "firebase";
+import "firebaseui/dist/firebaseui.css";
+import logo from "./images/logo+tag.jpg";
+import bloodimg from "./images/Blood-img.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const fbase = firebase.initializeApp(firebaseConfig);
+    const uiConfig = {
+      // signInSuccessUrl: "", //This URL is used to return to that page when we got success response for phone authentication.
+      signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
+      // tosUrl: "",
+    };
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }
+
+  _renderCounter = () => () => {
+    const [appState, setAppState] = useState("empty");
+
+    const SignOut = () => {
+      return (
+        firebase.auth().currentUser && (
+          <button
+            className="sign-out"
+            onClick={() => firebase.auth().signOut()}
+          >
+            Sign Out
+          </button>
+        )
+      );
+    };
+
+    useEffect(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          setAppState("edit");
+        } else {
+          setAppState("empty");
+        }
+      });
+    }, []);
+
+    return (
+      <div>
+        <nav>
+          <div class="topnav">
+            <img src={logo} alt="logo" className="logo" />
+            <div class="topnav-right">
+              <a href={"url"} className="hover-underline-animation">
+                Find Blood Donor
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                About
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                Contact
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                FAQs
+              </a>
+            </div>
+          </div>
+        </nav>
+        {appState === "empty" && (
+          <div>
+            <div className="register">
+              <p className="register-text">
+                Register or Sign In for Blood Donors
+              </p>
+              <img src={bloodimg} alt="blood-img" className="blood-img" />
+            </div>
+            <div id="firebaseui-auth-container"></div>
+          </div>
+        )}
+        {appState === "edit" && (
+          <div>
+            <h1>Hello</h1>
+            <SignOut />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  render() {
+    const MyInlineHook = this._renderCounter();
+    return (
+      <div className="container">
+        {/* <nav>
+          <div class="topnav">
+            <img src={logo} alt="logo" className="logo" />
+            <div class="topnav-right">
+              <a href={"url"} className="hover-underline-animation">
+                Find Blood Donor
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                About
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                Contact
+              </a>
+              <a href={"url"} className="hover-underline-animation">
+                FAQs
+              </a>
+            </div>
+          </div>
+        </nav> */}
+        {/* <div className="register">
+          <p className="register-text">Register or Sign In for Blood Donors</p>
+          <img src={bloodimg} alt="blood-img" className="blood-img" />
+        </div>
+        <div id="firebaseui-auth-container"></div> */}
+        <div>
+          <MyInlineHook />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDBwWrF0LVZzSo0TSRTFa1Xsw4HtrDX-Ww",
+  authDomain: "blood-donors-58841.firebaseapp.com",
+  projectId: "blood-donors-58841",
+  storageBucket: "blood-donors-58841.appspot.com",
+  messagingSenderId: "97832454555",
+  appId: "1:97832454555:web:546be1dc9cd4d08a1cb93e"
+};
+
+export default firebaseConfig;
