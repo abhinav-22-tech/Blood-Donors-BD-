5fb5d587e33ddfcdfb92efc76b09e366f6657e63
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-10-24 16:15:56 +0530

    contact details

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./contact.css";
 import { db } from "../../firebaseConfig";
-import { TextField, MenuItem } from "@mui/material";
+import {
+  TextField,
+  MenuItem,
+  Checkbox,
+  FormControlLabel,
+  Button,
+} from "@mui/material";
 import { CssBaseline, Container, Grid } from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
-
+import firebase from "firebase";
 import GeoLocation from "../GeoLocation/GeoLocation";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,23 +23,26 @@ const useStyles = makeStyles((theme) => ({
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
   const [bloodGroup, setBloodGroup] = useState("A+");
   const classes = useStyles();
   const [country, setCountry] = useState("");
   const [state, setState] = useState("");
   const [city, setCity] = useState("");
+  const [address, setAddress] = useState("");
+  const [authorize, setAuthorize] = useState("");
 
-  // console.log({
-  //   country,
-  //   state,
-  //   city,
-  // });
+  const [available, setAvailable] = useState("");
 
   const [loader, setLoader] = useState(false);
 
+  const [currentUser, setCurrentUser] = useState(null);
+  const [phoneno, setPhoneno] = useState("empty");
   const handleChange = (event) => {
     setBloodGroup(event.target.value);
+  };
+
+  const handleAvailableChange = (event) => {
+    setAvailable(event.target.value);
   };
   const bloodGroups = [
     {
@@ -110,6 +119,17 @@ const Contact = () => {
     },
   ];
 
+  const availability = [
+    {
+      value: "Available",
+      label: "Available",
+    },
+    {
+      value: "Unavailable",
+      label: "Unavailable",
+    },
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoader(true);
@@ -117,12 +137,15 @@ const Contact = () => {
     db.collection("contacts")
       .add({
         name: name,
-        email: email,
         bloodGroup: bloodGroup,
-        message: message,
+        phoneno: phoneno,
         country: country,
         state: state,
         city: city,
+        address: address,
+        email: email,
+        authorize: authorize,
+        available: available,
       })
       .then(() => {
         setLoader(false);
@@ -135,8 +158,21 @@ const Contact = () => {
 
     setName("");
     setEmail("");
-    setMessage("");
   };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+        setPhoneno(user.phoneNumber);
+      } else {
+        setCurrentUser(null);
+        setPhoneno("empty");
+      }
+    });
+  }, []);
+
+  // console.log("Current User" + currentUser?.phoneNumber);
 
   return (
     <>
@@ -144,15 +180,37 @@ const Contact = () => {
         <CssBaseline />
         <div className={classes.paper}>
           <form className={classes.form} onSubmit={handleSubmit}>
-            <TextField
-              id="outlined-basic"
-              label="Full Name"
-              variant="outlined"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  id="outlined-basic"
+                  label="Full Name"
+                  variant="outlined"
+                  fullWidth="true"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="outlined-select-currency"
+                  select
+                  label="Blood Group"
+                  value={bloodGroup}
+                  fullWidth="true"
+                  onChange={handleChange}
+                  helperText="Please select your Blood Group"
+                >
+                  {bloodGroups.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+
             <Grid container spacing={2}>
               <Grid item xs={12} sm={4}>
                 <GeoLocation
@@ -175,6 +233,87 @@ const Contact = () => {
                   onChange={setCity}
                   geoId={state}
                 />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-basic"
+                  label="Address"
+                  variant="outlined"
+                  fullWidth="true"
+                  placeholder="Address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  helperText="Please enter your current address"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-basic"
+                  label="Email Id"
+                  variant="outlined"
+                  fullWidth="true"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  helperText="Please enter your email id"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-basic"
+                  label="Phone Number"
+                  variant="outlined"
+                  fullWidth="true"
+                  placeholder="Phone Number"
+                  value={currentUser?.phoneNumber}
+                  disabled="disabled"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  select
+                  label="Please confirm your availability to donate blood"
+                  value={available}
+                  fullWidth="true"
+                  onChange={handleAvailableChange}
+                  helperText="Please select your Availability"
+                >
+                  {availability.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox />}
+                  onChange={(e) => setAuthorize(e.target.value)}
+                  label="I authorise this website to display my name and telephone number, so that the needy could contact me, as and when there is an emergency."
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained">
+                  Submit
+                </Button>
               </Grid>
             </Grid>
           </form>
